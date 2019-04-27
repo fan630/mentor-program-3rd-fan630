ecdb9e07d624fff80002dc760d6d07ff3e94856f
Norris <Norris@Norrisde-MacBook-Air.local>
2019-04-27 21:47:50 +0800

    revise

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,11 +1,16 @@
-function printStars (n) {
-  let answer = ''
-  for (let i = 1; i <= n; i++) {
-    answer += '*' + '\n'
-  }
-  console.log(answer)
-};
+// function printStars (n) {
+//   let answer = ''
+//   for (let i = 1; i <= n; i++) {
+//     answer += '*' + '\n'
+//   }
+//   console.log(answer)
+// };
 
-printStars(1)
+function printStars (n) {
+  for (let i = 1; i <= n; i++) {
+    console.log('*')
+  }
+}
+// printStars(1)
 printStars(3)
-printStars(6)
+// printStars(6)
